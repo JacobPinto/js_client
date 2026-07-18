8774b1cb8392a39f560ea50f0a97ea82fcfb5092
JacobPinto <pintobrijesh@gmail.com>
2026-07-18 17:48:44 +0000

    rm client side

--- m/modelObserver.ts
+++
@@ -1,9 +0,0 @@
-import { Dimensions, ShaderType, VertexFormat } from '../m/modelEnums.js';
-
-// dimension observer interfave
-// update method should be in the viewsimengine #TBD
-
-// Generic observer interface
-export interface Observer {
-  update(value: any): void;
-}
