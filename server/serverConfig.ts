bb80e056df7e1aabb71f58acf743b5c13d997b57
muzibashaikh <muzibashaikh@gmail.com>
2026-08-16 14:29:42 +0530

    DEV: changed workdir and binaryPath

--- server/serverConfig.ts
+++ server/serverConfig.ts
@@ -1,6 +1,8 @@
 export const ServerConfig = {
     port: 4000, // port changed from 3000 to 4000
     host: '0.0.0.0',  // Listen on all interfaces (allows WSL/external connections)
-    workdir: '/home/jacob137/testing/js_client/dist/workdir', // working directory for the server
-    binaryPath: '/home/jacob137/testing/simEngine/install/bin', // path to the Talos binary
+    //workdir: '/home/jacob137/testing/js_client/dist/workdir', // working directory for the server
+    //binaryPath: '/home/jacob137/testing/simEngine/install/bin', // path to the Talos binary
+    workdir: "D:/PROJECT/js_client/workdir",
+    binaryPath: '/home/muziba/install/bin' // path to the Talos binary
 }
